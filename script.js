48ff93d54695bc48d1a67d5d99cf38bf462331b6
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-08 00:19:15 -0300

    Major rewrite: game now successfully plays as it should!

--- script.js
+++ script.js
@@ -1,45 +1,76 @@
-// A function to toggle the rock, paper and scissor buttons as well as the play button1
-function toggleElementDisplay() {
-    let visible = false;
+// Initialize the variables for computer, user scores and some other
+let playerScore;
+let computerScore;
+let roundWinner;
+let visible = false;
 
+// Add menu and a button to the DOM
+const menuContainer = document.body.appendChild(document.createElement("div"))
+      menuContainer.setAttribute("class", "menu-container");
+const playBtn = menuContainer.appendChild(document.createElement("button"));
+      playBtn.setAttribute("class", "playButton")
+      playBtn.textContent = "Play!";
+// Another div that contains the rest of the buttons
+const gameContainer = menuContainer.appendChild(document.createElement("div"));
+      gameContainer.setAttribute("class", "gameDiv");
+const rockBtn = gameContainer.appendChild(document.createElement("button"));
+      rockBtn.setAttribute("class", "rockButton");
+      rockBtn.textContent = "Rock";
+const paperBtn = gameContainer.appendChild(document.createElement("button"));
+      paperBtn.setAttribute("class", "paperButton");
+      paperBtn.textContent = "Paper";
+const scissorsBtn = gameContainer.appendChild(document.createElement("button"));
+      scissorsBtn.setAttribute("class", "scissorsButton");
+      scissorsBtn.textContent = "Scissors";
+
+const scorePara = document.querySelector("#score");
+
+playBtn.addEventListener("click", () => { playGame() });
+
+// A naive function to toggle the rock, paper and scissor buttons as well as the play button
+function toggleElementDisplay() {
     if( visible === false ) {
-        playButton.style.visibility = "hidden";
-        gameDiv.style.visibility = "visible"; 
+        playBtn.style.visibility = "hidden";
+        gameContainer.style.visibility = "visible"; 
         visible = true;
     } else {
-        playButton.style.visibility = "visible";
-        gameDiv.style.visibility = "hidden";
+        playBtn.style.visibility = "visible";
+        gameContainer.style.visibility = "hidden";
         visible = false;
     }
-}
+};
 
-// A function that randomly returns string values: "rock", "paper", "scissors"
-function getComputerChoice() {
-    // Get a very simple, evenly distributed random between 0 and 100 as integers 
-    let rand = Math.floor(Math.random() * 100);
-    let str = rand < 33 ? "rock" : 
-              rand >= 33 && rand < 66 ? "paper" : "scissors";
+// Lets listen to a click in the div block
+gameContainer.addEventListener("click", (event) => {
+    let button = event.target;
+    let buttonClass = button.classList.value;
+
+    switch (buttonClass) {
+        case "rockButton": 
+            playerChoice = "rock";
+            return playRound(playerChoice);
+        case "paperButton":
+            playerChoice = "paper";
+            return playRound(playerChoice);
+        case "scissorsButton":
+            playerChoice = "scissors";
+            return playRound(playerChoice);
+        default: 
+            break;
+    }
+});
+
+// A function to get a randomized string 
+function getComputerChoice() { 
+    let rand = Math.floor(Math.random() * 3);
+    let str = rand < 1 ? "rock" : rand < 2 ? "paper" : "scissors";
 
     return str;
-}
+};
 
-// A funtion that simply returns whatever button the player clicked
-function getPlayerChoice() {
-    switch(buttonString) {
-        case "rockButton":
-            return "rock";
-        case "paperButton":
-            return "paper";
-        case "scissorsButton":
-            return "scissors";
-        default :
-            return ;
-    }
-}
-
-// Lets create a round
-function playRound(playerChoice, computerChoice) {
-    if( playerChoice === null || playerChoice === undefined ) { return ; }
+// Lets have a round!
+function playRound(playerChoice) {
+    const computerChoice = getComputerChoice();
 
     roundWinner = (playerChoice === "rock" && computerChoice === "scissors") ? "Player" :
                     (playerChoice === "rock" && computerChoice === "paper") ? "Computer" :
@@ -50,104 +81,38 @@ function playRound(playerChoice, computerChoice) {
 
     switch(roundWinner) {
         case "Player":
-            alert(`You won! ${playerChoice} beats ${computerChoice}.`)
+            scorePara.textContent = `You won! ${playerChoice} beats ${computerChoice}.`;
             playerScore += 1;
             break;
         case "Computer":
-            alert(`You lose! ${computerChoice} beats ${playerChoice}.`)
+            scorePara.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
             computerScore += 1;
             break;
         case "Tie":
-            alert(`You were on a tie. Must try again!`)
+            scorePara.textContent = `You were on a tie. Must try again!`;
             break;
     }
-    return roundWinner;
-}
 
-// Lets play a game 5 times
-function playGame() {
-    // Keep track of the players' scores
-    playerScore = 0;
-    computerScore = 0;
-
-    while( Math.max(playerScore, computerScore) < 5 ) {
-        const playerChoice = getPlayerChoice();
-        const computerChoice = getComputerChoice();
-
-        playRound(playerChoice, computerChoice);
-
-        // Escape early if the game was nullified
-        if( playerChoice === null || playerChoice === undefined ) { return ; } 
-    }
-
-    let msg = playerScore > computerScore ? "You won the game! " : "You lost the game! ";
-    document.querySelector("#score").textContent = msg + `You scored ${playerScore} vs ${computerScore}.`;
-
-    return ;
-}
-/*function playGame() {
-    // Reset these variables once we enter the game 
-    playerScore = 0;
-    computerScore = 0;
-    let i = 0;
-
-    while(i < 5) {
-        const playerChoice = getPlayerChoice();
-        const computerChoice = getComputerChoice();
-
-        // Escape early if the game was canceled
-        if( playerChoice === null || playerChoice === undefined ) { return ; } 
-
-        playRound(playerChoice, computerChoice);
-
-        // Do increment the index if it was not a tie or something else
-        if( roundWinner === "Player" || roundWinner === "Computer" ) { i++; }
-    }
-    
-    let msg = playerScore > computerScore ? "You win! " : "You lose! ";
-    document.getElementById("score").innerHTML = msg + `You scored ${playerScore} vs ${computerScore}.`;
-
-    return ;
-}*/
-
-// Initialize the variables for computer, user scores and some other
-let playerScore;
-let computerScore;
-let roundWinner;
-let buttonString;
-
-// Add a div menu and a button to the HTML body
-const menuButtons = document.body.appendChild(document.createElement("div"));
-      menuButtons.setAttribute("class", "menu-container")
-const playButton = menuButtons.appendChild(document.createElement("button"));
-      playButton.setAttribute("class", "playButton");
-      playButton.textContent = "Play!";
-
-// The other div that contains the rest of the buttons
-const gameDiv = menuButtons.appendChild(document.createElement("div"));
-      gameDiv.setAttribute("class", "gameDiv");
-const rockButton = gameDiv.appendChild(document.createElement("button"));
-      rockButton.setAttribute("class", "rockButton");
-      rockButton.textContent = "Rock";
-const paperButton = gameDiv.appendChild(document.createElement("button"));
-      paperButton.setAttribute("class", "paperButton");
-      paperButton.textContent = "Paper";
-const scissorsButton = gameDiv.appendChild(document.createElement("button"));
-      scissorsButton.setAttribute("class", "scissorsButton");
-      scissorsButton.textContent = "Scissors";
-
-// Now we listen for any clicks inside the menu div box
-menuButtons.addEventListener("click", (event) => {
-
-    let button = event.target;
-    buttonString = button.classList.value;
-
-    if( buttonString == "playButton" ) {
-        playButton.textContent = "Try again!";
-        // Switch visibility of the game buttons
+    // Once either the player or computer scores 5, the game ends
+    if( Math.max(playerScore, computerScore) >= 5 ) {
         toggleElementDisplay();
+        playBtn.textContent = "Try again!";
+
+        let msg = playerScore > computerScore ? "You won the game! " : "You lost the game! ";
+        scorePara.textContent = msg + `You scored ${playerScore} vs ${computerScore}.`;
     }
-    else if( buttonString == "rockButton" || 
-             buttonString == "paperButton" || 
-             buttonString == "scissorsButton" ) { playGame() }
-});
+
+    return roundWinner;
+};
+
+// Lets play a game!
+function playGame() {
+    // Reset the scores 
+    playerScore = 0;
+    computerScore = 0;
+
+    toggleElementDisplay();
+
+    scorePara.textContent = "Choose either rock, paper or scissors";
+    return ;
+};
